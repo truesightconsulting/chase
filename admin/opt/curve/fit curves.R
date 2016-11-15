56c372bb7d8653f84703737891269bacc849ab37
yoruhyde <yuemeng1@USNYCZOG8351.global.publicisgroupe.net>
2016-11-15 16:09:21 -0500

    fix cpp

--- admin/opt/curve/fit curves.R
+++ admin/opt/curve/fit curves.R
@@ -1,9 +1,9 @@
 library(data.table);library(bit64)
-setwd("d:\\Archives\\Git\\chase\\admin\\opt\\curve\\")
+setwd("C:\\Users\\yuemeng1\\Desktop\\TOOL\\chase\\admin\\opt\\curve\\")
 learn.rate.start=1e-6 # the start point of learn rate for fitting curves (vary from curve file to curve file)
 for.opt=T # T if the fitting is for otptimization curves
 
 ex.curve=fread("sim_modelinput_curve.csv")
 ex.dim=fread("sim_input_setup.csv")
 
-source("d:\\Archives\\Git\\curve-simulation\\curve simulator from ad.R")
+source("C:\\Users\\yuemeng1\\Desktop\\code\\curve-simulation\\curve simulator from ad.R")
