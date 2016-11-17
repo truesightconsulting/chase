a776ba3d55f0958e4882cc4fe90f4819fff8c221
yoruhyde <yuemeng1@USNYCZOG8351.global.publicisgroupe.net>
2016-11-17 12:34:36 -0500

    weekly curves

--- admin/opt/curve/fit curves.R
+++ admin/opt/curve/fit curves.R
@@ -2,6 +2,7 @@ library(data.table);library(bit64)
 setwd("C:\\Users\\yuemeng1\\Desktop\\TOOL\\chase\\admin\\opt\\curve\\")
 learn.rate.start=1e-6 # the start point of learn rate for fitting curves (vary from curve file to curve file)
 for.opt=T # T if the fitting is for otptimization curves
+is.refit=F
 
 ex.curve=fread("sim_modelinput_curve.csv")
 ex.dim=fread("sim_input_setup.csv")
